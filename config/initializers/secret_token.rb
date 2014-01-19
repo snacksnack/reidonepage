7b3bfe8db86f6615bf3e02ef21f6006c88856839
Reid Collins <velopants@gmail.com>
2014-01-19 17:41:25 -0500

    remove secret_token from gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reidonepage::Application.config.secret_token = '9aeceeabebcee8c2c4da6f86b76aa96f7528a6b520c9d440d6545d3ee9c0528146dcf4ba1fdd5e9f97f9795045cea37bfe7c128a723e05a817238e79ac23b6d5'

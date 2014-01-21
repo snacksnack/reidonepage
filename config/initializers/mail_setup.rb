a777e9e34d4a7f90894d38ebb90571989165a2c7
Reid Collins <velopants@gmail.com>
2014-01-21 14:59:21 -0500

    address gmail authen warning

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
 	address: "smtp.gmail.com",
 	port: '587',
 	domain: 'hihelloreid.com',
-	user_name: 'hihelloreid',
+	user_name: 'hihelloreid@gmail.com',
 	password: 'franz13!',
 	authentication: 'plain',
 	enable_starttls_auto: true

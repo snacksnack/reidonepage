9aa2eab91160c4fe1f47870fe48a5c278ff5b30d
Reid Collins <velopants@gmail.com>
2014-01-21 14:47:06 -0500

    add email notifcation for new client. update doc and pdf of resume.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -11,13 +11,12 @@ class ClientsController < ApplicationController
     	if @client.save
         format.html { redirect_to root_path, flash[:notice] = "Thanks, I'll be in touch shortly!" }
         format.js
+        ClientMailer.client_notice(@client).deliver
     	else
     		flash[:notice] = "Oops. Something's gone wrong!"
         format.html
         format.js { }
     	end
+    end
   end
-  	
-  end
-  
 end

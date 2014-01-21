49a30044d7c092369c8f0ce0a8780924ccc90c14
Reid Collins <velopants@gmail.com>
2014-01-21 09:29:52 -0500

    change heroku ping to every minute

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -2,9 +2,10 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.new
 
 if Rails.env.production?
-  scheduler.every '10m' do
+  scheduler.every '1m' do
      require "net/http"
      require "uri"
+     pust "#{HOSTNAME}"
      Net::HTTP.get_response(URI.parse(ENV["HOSTNAME"]))
   end
 end
